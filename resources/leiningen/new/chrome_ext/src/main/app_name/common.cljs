b9dc01e69a900528c0474b9f58f978f02edb1289
Lih Chen <lihster@gmail.com>
2025-06-20 15:40:28 -0700

    handles storage

--- resources/leiningen/new/chrome_ext/src/main/app_name/common.cljs
+++ resources/leiningen/new/chrome_ext/src/main/app_name/common.cljs
@@ -1,6 +1,7 @@
 (ns {{name}}.common
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs.core.async.interop :refer-macros [<p!]]
+            [cljs.core.async :refer [<!]]
             [cognitect.transit :as t]
             ))
 
@@ -16,6 +17,30 @@
   (go
     (let [t (<p! (js/chrome.tabs.query #js{:lastFocusedWindow true :active true}))]
       (-> t js->clj first (get "id"))
+      )))
+
+(defn set-local-storage
+  "
+  Example: (go (let [r (<! (set-local-storage \"foo\" {\"a\" \"a\" \"b\" \"b\"}))]))
+  "
+  [k v]
+  (go
+    (<p! (js/chrome.storage.local.set (clj->js {k v})))))
+
+(defn get-local-storage
+  "
+  Example:
+  (go (let [data (<! (get-local-storage \"foo\"))]
+      (prn \">> data: \" (get data \"a\"))
+      ))
+  "
+  [k]
+  (go
+    (let [data (<p! (js/chrome.storage.local.get (clj->js [k])))
+          data (js->clj data)
+          ]
+      (when-not (empty? data)
+        (get data k))
       )))
 
 ;; (go (prn (<! (get-active-tab-id))))
